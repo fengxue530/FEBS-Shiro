ccca50cfa832c02aa1d09a817379b3eef97412db
mabaofeng <oneinchofsunshine@163.com>
2019-07-05 15:51:44 +0800

    pom 增加 war包参数，并可使用tomcat 运行

--- src/main/java/cc/mrbird/febs/FebsApplication.java
+++ src/main/java/cc/mrbird/febs/FebsApplication.java
@@ -3,6 +3,8 @@ package cc.mrbird.febs;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,7 +15,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAsync
 @EnableTransactionManagement
 @MapperScan("cc.mrbird.febs.*.mapper")
-public class FebsApplication {
+/***Feign 服务端 注解*/
+@EnableEurekaServer
+public class FebsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FebsApplication.class);
+    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(FebsApplication.class)
